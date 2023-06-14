e59bd844ad5037f2b54599584190a81b7bd567bd
KaspervanM <kaspervanm@gmail.com>
2023-06-14 19:47:10 +0200

    test: the version where output is sensible, but batches don't work yet
    Signed-off-by: KaspervanM <kaspervanm@gmail.com>

--- NN.py
+++ NN.py
@@ -1,33 +1,16 @@
 """https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html"""
-import matplotlib.pyplot as plt
 import torch
 from torch import nn
 from torch import Tensor
 from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
-
 from ImageDataset import ImageDataset
-from interface import blur_tensor
-from interface import polarize_output
-
-SHOULD_TRAIN = True
-LOAD_MODEL = False
-EPOCHS = 100000
-LEARNING_RATE = 5e-3
-BATCH_SIZE = 3
-SEED = 0
-BATCH_DISPLAY_INTERVAL = None
-LOSS_FUNCTION = nn.MSELoss()  # nn.CrossEntropyLoss()
-DIR_TRAINING_DATA = "input/train"
-DIR_TEST_DATA = "input/train"
-SAVE_MODEL_NAME = f"model2_e{EPOCHS}_l{LEARNING_RATE}_b{BATCH_SIZE}_s{SEED}_lf{LOSS_FUNCTION._get_name()}.pth"
-LOAD_MODEL_NAME = SAVE_MODEL_NAME.replace(f"_e{EPOCHS}",
-                                          "_e11526") + ".interrupted"
+from interface import blur_tensor, polarize_output
+import matplotlib.pyplot as plt
 
 
 # Define model
 class NeuralNetwork(nn.Module):
-
     def __init__(self):
         super().__init__()
         self.flatten = nn.Flatten()
@@ -35,187 +18,119 @@ class NeuralNetwork(nn.Module):
             nn.Linear(512 * 512, 512),
             nn.ReLU(),
             nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.Hardsigmoid(),
-            nn.Linear(512, 512 * 512),
+            nn.Sigmoid(),
+            nn.Linear(512, 512 * 512)
         )
 
     def forward(self, x) -> Tensor:
-        """Forward pass of the model.
-
-        :param x: The input tensor.
-        :return: The output tensor.
-        """
         x = self.flatten(x)
-        return self.linear_stack(x)
+        logits = self.linear_stack(x)
+        return logits
 
 
-def train(dataloader, network_model: NeuralNetwork, loss_fn,
-          network_optimizer):
-    """Trains the model on the given dataloader.
-
-    :param dataloader: The dataloader to train on.
-    :param network_model: The model to train.
-    :param loss_fn: The loss function to use.
-    :param network_optimizer: The optimizer to use.
-    """
+def train(dataloader, model, loss_fn: nn.MSELoss, optimizer):
     size = len(dataloader.dataset)
-    network_model.train()
+    model.train()
     for batch, sample in enumerate(dataloader):
-        inputs = sample["image"].to(device)
+        inp = sample['image'].to(device)
 
         # Compute prediction error
-        predictions = network_model(inputs)
-        loss = loss_fn.forward(blur_tensor(predictions), blur_tensor(inputs))
+        pred = model(inp)
+        loss = loss_fn.forward(blur_tensor(pred).flatten(), blur_tensor(inp).flatten())
+
+        # print(f"loss: {loss}")
 
         # Backpropagation
         loss.backward()
-        network_optimizer.step()
-        network_optimizer.zero_grad()
+        # for i, param in enumerate(model.parameters()):
+        #     print(f"grad {i}: {param.grad}")
+        optimizer.step()
+        optimizer.zero_grad()
 
-        if BATCH_DISPLAY_INTERVAL is not None:
-            if batch % BATCH_DISPLAY_INTERVAL == 0:
-                current = (batch + 1) * len(blur_tensor(inputs))
-                print(f"loss: {loss.item():>7f}  [{current:>5d}/{size:>5d}]")
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(blur_tensor(inp))
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
 
 
-def test(dataloader,
-         network_model: NeuralNetwork,
-         loss_fn,
-         show_img: bool = False):
-    """Tests the model on the given dataloader.
-
-    :param dataloader: The dataloader to test on.
-    :param network_model: The model to test.
-    :param loss_fn: The loss function to use.
-    :param show_img: Whether to show the image or not.
-    """
+def test(dataloader, model: NeuralNetwork, loss_fn, show_img: bool = False):
+    size = len(dataloader.dataset)
     num_batches = len(dataloader)
-    network_model.eval()
+    model.eval()
     test_loss, correct = 0, 0
     with torch.no_grad():
         for sample in dataloader:
-            inputs = sample["image"].to(device)
-            predictions = network_model.forward(inputs)
+            inp = sample['image'].to(device)
+            pred = model.forward(inp)
             if show_img:
-                show_images(sample, predictions)
-            test_loss += loss_fn.forward(blur_tensor(predictions),
-                                         blur_tensor(inputs)).item()
+                print(polarize_output(pred.cpu()).view(512, 512))
+                plt.imshow(polarize_output(pred.cpu()).view(512, 512), cmap='gray', interpolation='nearest', vmin=0, vmax=1)
+                plt.show()
+                plt.imshow(blur_tensor(polarize_output(pred.cpu())).view(512, 512), cmap='gray', vmin=0, vmax=1)
+                plt.show()
+                plt.imshow(blur_tensor(inp.cpu()).view(512, 512), cmap='gray', vmin=0, vmax=1)
+                plt.show()
+            test_loss += loss_fn(blur_tensor(pred).flatten(),
+                                 blur_tensor(inp).flatten()).item()
+            # correct += (blur_tensor(pred).argmax(1) == blur_tensor(inp)).type(torch.float).sum().item()
     test_loss /= num_batches
+    # correct /= size
+    # print(f"Test Error: \n Accuracy: {(100 * correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
     print(f"Test Error: Avg loss: {test_loss:>8f} \n")
 
 
-def show_images(sample, predictions):
-    """Shows the images in the given sample.
-
-    :param sample: The sample to show.
-    :param predictions: The predictions to show.
-    """
-    for filename, sample, predictions in zip(sample["filename"],
-                                             sample["image"], predictions):
-        show_image_single(filename, sample, predictions)
-
-
-def show_image_single(filename,
-                      original,
-                      prediction,
-                      image_shape: tuple[int, int] = (512, 512)):
-    """Shows the image with the given filename, original and prediction.
-
-    :param filename: The filename of the image.
-    :param original: The original image.
-    :param prediction: The prediction of the image.
-    :param image_shape: The shape of the image.
-    """
-    fig = plt.figure(figsize=(5, 12))
-    fig.add_subplot(3, 1, 1)
-    plt.imshow(
-        polarize_output(prediction.cpu()).view(image_shape),
-        cmap="gray",
-        interpolation="nearest",
-        vmin=0,
-        vmax=1,
-    )
-    plt.title(f"{filename}: prediction")
-    fig.add_subplot(3, 1, 2)
-    plt.imshow(blur_tensor(prediction.cpu()).view(image_shape),
-               cmap="gray",
-               vmin=0,
-               vmax=1)
-    plt.title(f"{filename}: prediction blurred")
-    fig.add_subplot(3, 1, 3)
-    plt.imshow(blur_tensor(original.cpu()).view(image_shape),
-               cmap="gray",
-               vmin=0,
-               vmax=1)
-    plt.title(f"{filename}: original blurred")
-    plt.tight_layout(h_pad=1.0)
-    plt.show()
-
-
-if __name__ == "__main__":
-    torch.manual_seed(SEED)
-    torch.cuda.manual_seed(SEED)
-
+if __name__ == '__main__':
     # Download training data from open datasets.
-    training_data = ImageDataset(DIR_TRAINING_DATA, transform=ToTensor())
+    training_data = ImageDataset('input/train', transform=ToTensor())
 
     # Download test data from open datasets.
-    test_data = ImageDataset(DIR_TEST_DATA, transform=ToTensor())
+    test_data = ImageDataset('input/train', transform=ToTensor())
+
+    batch_size = 1
 
     # Create data loaders.
-    train_dataloader = DataLoader(training_data,
-                                  batch_size=BATCH_SIZE,
-                                  shuffle=True)
-    test_dataloader = DataLoader(test_data,
-                                 batch_size=BATCH_SIZE,
-                                 shuffle=True)
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
 
     for test_sample in test_dataloader:
         print(f"Shape of image [N, C, H, W]: {test_sample['image'].shape}")
-        print(f"filename of image: {test_sample['filename']}")
+        print(f"Shape of blurred image: {test_sample['blurred image'].shape} {test_sample['blurred image'].dtype}")
         break
 
-    if not SHOULD_TRAIN:
-        device = "cpu"
-        model = NeuralNetwork().to(device)
-        model.load_state_dict(torch.load(LOAD_MODEL_NAME))
-        test(test_dataloader, model, LOSS_FUNCTION, show_img=True)
-        exit(0)
-
     # Get cpu, gpu or mps device for training.
-    device = ("cuda" if torch.cuda.is_available() else
-              "mps" if torch.backends.mps.is_available() else "cpu")
+    device = (
+        "cuda"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    # device = "cpu"
     print(f"Using {device} device")
 
     model = NeuralNetwork().to(device)
-    if LOAD_MODEL:
-        model.load_state_dict(torch.load(LOAD_MODEL_NAME))
-        print(f"Loaded PyTorch Model State from {LOAD_MODEL_NAME}")
+    for param in model.parameters():
+        param.requires_grad = True
+
+    for name, param in model.named_parameters():
+        print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
     print(model)
-    optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE)
-    epochs_completed = 0
-    if LOAD_MODEL:
-        epochs_completed = int(LOAD_MODEL_NAME.split("_e")[1].split("_")[0])
-    try:
-        for t in range(epochs_completed, EPOCHS):
-            print(f"Epoch {t + 1}\n-------------------------------")
-            train(train_dataloader, model, LOSS_FUNCTION, optimizer)
-            test(test_dataloader, model, LOSS_FUNCTION)
-            epochs_completed += 1
-        print("Done!")
-    except KeyboardInterrupt:
-        print("You pressed CTRL + C.")
-        print("Program interrupted.")
-        interrupted_model_name = (
-            SAVE_MODEL_NAME.replace(f"_e{EPOCHS}", f"_e{epochs_completed}") +
-            ".interrupted")
-        torch.save(model.state_dict(), interrupted_model_name)
-        print(f"Saved PyTorch Model State to {interrupted_model_name}")
-        exit(0)
+    loss_fn = nn.MSELoss()  # nn.CrossEntropyLoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-1)
+    epochs = 2000
+    for t in range(epochs):
+        print(f"Epoch {t + 1}\n-------------------------------")
+        train(train_dataloader, model, loss_fn, optimizer)
+        test(test_dataloader, model, loss_fn)
+    print("Done!")
+    test(test_dataloader, model, loss_fn, show_img=True)
+    for name, param in model.named_parameters():
+        print(f"Layer: {name} | Size: {param.size()} | Values : {param[:2]} \n")
 
-    torch.save(model.state_dict(), SAVE_MODEL_NAME)
-    print(f"Saved PyTorch Model State to {SAVE_MODEL_NAME}")
-
-    test(test_dataloader, model, LOSS_FUNCTION, show_img=True)
+    torch.save(model.state_dict(), "model.pth")
+    print("Saved PyTorch Model State to model.pth")
+    #
+    # device = "cpu"
+    # model = NeuralNetwork().to(device)
+    # model.load_state_dict(torch.load("model.pth"))
+    # loss_fn = nn.MSELoss()
+    # test(test_dataloader, model, loss_fn, show_img=True)
--- interface.py
+++ interface.py
@@ -1,29 +1,17 @@
 """This module provides an interface to the c library of netpbm-c."""
 import os
 from ctypes import *
-
 import numpy as np
-from torch import Tensor
 from torch import tensor
+from torch import Tensor
 from torchvision.transforms import GaussianBlur
 
-__all__ = [
-    "get_normalized_images_training_data_from_directory",
-    "polarize_output",
-    "blur_image",
-    "blur_tensor",
-]
+__all__ = ['get_normalized_images_training_data_from_directory', 'polarize_output', 'blur_image', 'blur_tensor']
 
 
 class PgmImage(Structure):
     """The PgmImage struct used by the c library."""
-
-    _fields_ = [
-        ("width_", c_uint32),
-        ("height_", c_uint32),
-        ("max_gray_", c_uint16),
-        ("data_", POINTER(c_uint8)),
-    ]
+    _fields_ = [("width_", c_uint32), ("height_", c_uint32), ("max_gray_", c_uint16), ("data_", POINTER(c_uint8))]
 
 
 # Load the shared library into c types.
@@ -46,34 +34,36 @@ ctest.WritePgm.argtypes = [c_char_p, POINTER(PgmImage)]
 ctest.WritePgm.restype = c_bool
 
 
-def get_normalized_images_training_data_from_directory(
-    directory: str, expected_size: tuple = (512, 512)
-) -> list[tuple[str, np.ndarray]]:
+def get_normalized_images_training_data_from_directory(directory: str, expected_size: tuple = (512, 512),
+                                                       radius: int = 1) -> np.ndarray:
     """Reads the data of all images in the specified directory.
 
     The data is normalized to values between 0 and 1.
     It is assumed that the images are pgm images.
+    training data is formed from the images by blurring them with a given radius.
     :param directory: The directory to read the images from.
     :param expected_size: The expected size of the images.
-    :return: A list containing a tuple with the image filename and the normalized image data.
+    :param radius: The radius to blur the images with.
+    :return: A numpy array containing a tuple with the normalized image data and corresponding blurred image.
     """
     images = []
     for filename in os.listdir(directory):
         if filename.endswith(".pgm"):
-            img = ctest.ReadPgm(
-                os.path.join(directory, filename).encode("utf-8"))
-            if (img.contents.width_ != expected_size[0]
-                    or img.contents.height_ != expected_size[1]):
+            img = ctest.ReadPgm(os.path.join(directory, filename).encode("utf-8"))
+            if img.contents.width_ != expected_size[0] or img.contents.height_ != expected_size[1]:
                 print(
                     f"An image that is not {expected_size[0]}x{expected_size[1]} was found: {filename}"
-                    f" is {img.contents.width_}x{img.contents.height_}")
+                    f" is {img.contents.width_}x{img.contents.height_} instead of 512")
                 continue
             normalized_img = ctest.NormalizePgm(img)
-            input_data_image = np.ctypeslib.as_array(
-                normalized_img, shape=(expected_size[0] *
-                                       expected_size[1], )).astype(np.double)
-            images.append((filename, input_data_image))
-    return images
+            input_data_image = np.ctypeslib.as_array(normalized_img,
+                                                     shape=(expected_size[0] * expected_size[1],)).astype(np.double)
+            target_img = ctest.KasperBlur(img, radius)
+            normalized_target_img = ctest.NormalizePgm(target_img)
+            target_data_image = np.ctypeslib.as_array(normalized_target_img,
+                                                      shape=(expected_size[0] * expected_size[1],)).astype(np.double)
+            images.append((input_data_image, target_data_image))
+    return np.array(images)
 
 
 def polarize_output(output: Tensor) -> Tensor:
@@ -85,9 +75,7 @@ def polarize_output(output: Tensor) -> Tensor:
     return tensor((output.detach().numpy() > 0.5).astype(np.float32))
 
 
-def blur_image(image: Tensor,
-               radius: int = 1,
-               shape: tuple = (512, 512)) -> Tensor:
+def blur_image(image: Tensor, radius: int = 1, shape: tuple = (512, 512)) -> Tensor:
     """Blurs the given image with the given radius.
 
     :param image: The image to blur.
@@ -95,41 +83,19 @@ def blur_image(image: Tensor,
     :param shape: The shape of the image.
     :return: The blurred image.
     """
-    img = PgmImage(
-        width_=shape[0],
-        height_=shape[1],
-        max_gray_=255,
-        data_=image.detach().numpy().astype(np.uint8).ctypes.data_as(
-            POINTER(c_uint8)),
-    )
+    img = PgmImage(width_=shape[0], height_=shape[1], max_gray_=255,
+                   data_=image.detach().numpy().astype(np.uint8).ctypes.data_as(POINTER(c_uint8)))
     blurred_img = ctest.KasperBlur(img, radius)
     normalized_blurred_img = ctest.NormalizePgm(blurred_img)
-    return tensor(
-        np.ctypeslib.as_array(normalized_blurred_img,
-                              shape=(shape[0] * shape[1], )).astype(
-                                  np.float32))
+    return tensor(np.ctypeslib.as_array(normalized_blurred_img, shape=(shape[0] * shape[1],)).astype(np.float32))
 
 
-def blur_tensor(
-        output: Tensor,
-        image_shape: tuple[int, int] = (512, 512),
-        kernel_size: float = 5,
-        sigma: tuple[float, float] = (5, 5),
-) -> Tensor:
+def blur_tensor(output: Tensor, kernel_size: float = 5, sigma: tuple[float, float] = (5, 5)) -> Tensor:
     """Blurs the given tensor with the given kernel size and sigma.
 
     :param output: The tensor to blur.
-    :param image_shape: The shape of the image.
     :param kernel_size: The kernel size to use.
     :param sigma: The sigma to use.
     :return: The blurred tensor.
     """
-    try:
-        return GaussianBlur(kernel_size,
-                            sigma=sigma)(output.view(output.shape[0],
-                                                     image_shape[0],
-                                                     image_shape[1]))
-    except RuntimeError:
-        return GaussianBlur(kernel_size,
-                            sigma=sigma)(output.view(1, image_shape[0],
-                                                     image_shape[1]))
+    return GaussianBlur(kernel_size, sigma=sigma)(output.view(1, 512, 512)).view(512 * 512)
